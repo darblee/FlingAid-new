6073eebbc5dd8838f22031e201c85bc2d0c90260
david-lee <darblee123@gmail.com>
2024-07-01 19:36:21 -0700

    Upgrade gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.4.1" apply false
+    id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23"
 }
